2fea35f9b7830d4c423f0d1e33bbd3d41d674d98
iambasantarai <iambasantarai@gmail.com>
2024-06-28 19:32:57 +0545

    Style & print application name with figlet

---
+++ src/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+'use strict';
+
+import figlet from "figlet";
+
+process.stdout.write('\x1Bc');
+console.log(
+    figlet.textSync('talktome', {
+        font: 'Small Slant',
+    }),
+);
+
