0acf6e23fe79f2110449a322760b60e29d26328b
iambasantarai <iambasantarai@gmail.com>
2024-06-28 20:36:18 +0545

    Take user credentials using inquirer and implement login

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import path from 'path';
 import { readFileSync } from 'fs';
 
 import figlet from 'figlet';
+import inquirer from 'inquirer';
+import { IgApiClient } from 'instagram-private-api';
 
 const pkgPath = path.join(process.cwd(), 'package.json');
 const pkg = JSON.parse(readFileSync(pkgPath, 'utf8'));
@@ -17,3 +19,32 @@ console.log(
   }),
 );
 console.log(`Version: ${pkg.version}`);
+
+const ig = new IgApiClient();
+
+const authenticator = async () => {
+  const { username, password } = await inquirer.prompt([
+    {
+      name: 'username',
+      type: 'input',
+      prefix: '>',
+      message: 'Enter your username: ',
+    },
+    {
+      name: 'password',
+      type: 'password',
+      mask: '*',
+      prefix: '>',
+      message: 'Enter your password: ',
+    },
+  ]);
+
+  ig.state.generateDevice(username);
+
+  return await ig.account.login(username, password);
+};
+
+(async () => {
+  const loggedInUser = await authenticator();
+  console.log('\r Logged in as ', loggedInUser.username);
+})();
